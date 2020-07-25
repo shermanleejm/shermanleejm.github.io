c27cf6e16dde6bd172e4cba26afe6451945f7877
leesherman <leesherman@accredify.io>
2020-07-26 01:27:32 +0800

    added real production build

---
+++ build/precache-manifest.65f8c29c583d04f395c49859d47118a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ee1228ad51f7177a515dc2885f1733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d7ba8a448169a6f784",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a4c2bf63377d50bbbc0",
+    "url": "/static/js/2.49f02753.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.49f02753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d7ba8a448169a6f784",
+    "url": "/static/js/main.92526b1c.chunk.js"
+  },
+  {
+    "revision": "6d4161236108dbafb771",
+    "url": "/static/js/runtime-main.7a9007fe.js"
+  },
+  {
+    "revision": "67b2575b500660f0bd96ed04d0ef9970",
+    "url": "/static/media/Resume.67b2575b.pdf"
+  }
+]);
