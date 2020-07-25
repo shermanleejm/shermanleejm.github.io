8bc2f51f763993c4ccf4659ac13059ba068ae506
leesherman <leesherman@accredify.io>
2020-07-26 01:42:22 +0800

    moved build files into ./

---
+++ precache-manifest.aafd0ddbdb68227eb733b42bd6b2a021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56795e53d3c95fa7c1390a586e77eb81",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3f7cada602df6fcc370e",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a4c2bf63377d50bbbc0",
+    "url": "/portfolio/static/js/2.49f02753.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.49f02753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7cada602df6fcc370e",
+    "url": "/portfolio/static/js/main.22dd4784.chunk.js"
+  },
+  {
+    "revision": "d237955457c84bacf55b",
+    "url": "/portfolio/static/js/runtime-main.c1c2f348.js"
+  },
+  {
+    "revision": "67b2575b500660f0bd96ed04d0ef9970",
+    "url": "/portfolio/static/media/Resume.67b2575b.pdf"
+  }
+]);
