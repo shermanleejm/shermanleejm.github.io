74192573e02ba97771516f407ee42d1b16c3c363
Sherman Lee <sherman.lee@accredify.io>
2022-01-25 15:48:49 +0800

    updated deck builder

--- src/components/MTGDB/DeckBuilder/DeckList.tsx
+++ src/components/MTGDB/DeckBuilder/DeckList.tsx
@@ -4,9 +4,10 @@ import {
   CardActionArea,
   CircularProgress,
   Grid,
+  TextField,
   Typography,
 } from "@mui/material";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CardsTableType } from "../../../database";
 
 type DeckListType = {
@@ -17,6 +18,9 @@ type DeckListType = {
 const DeckList = (props: DeckListType) => {
   const [cards, setCards] = useState<CardsTableType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [deckName, setDeckName] = useState<string>("");
+  const [isGenerating, setIsGenerating] = useState(true);
+  const [exportJson, setExportJson] = useState<string>("");
 
   useEffect(() => {
     function init() {
@@ -33,14 +37,42 @@ const DeckList = (props: DeckListType) => {
     <div>
       <Grid container justifyContent={"center"} alignItems={"flex-start"}>
         <Grid item xs={12}>
-          <Button
-            href={`data:text/json;charset=utf-8,${encodeURIComponent(
-              JSON.stringify(Array.from(props.cards))
-            )}`}
-            download={`MTGDB_dump_${Date.now()}.json`}
-          >
-            Export json
-          </Button>
+          <TextField
+            size="small"
+            fullWidth
+            label={"Deck Name"}
+            value={deckName}
+            onChange={(
+              e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ) => setDeckName(e.target.value)}
+          ></TextField>
+        </Grid>
+        <Grid item xs={12}>
+          {isGenerating ? (
+            <Button
+              onClick={() => {
+                let tmp = cards.map((c) => {
+                  let tmp = c;
+                  tmp.tags.push(deckName);
+                  return tmp;
+                });
+                setExportJson(JSON.stringify(Array.from(new Set(tmp))));
+                setIsGenerating(false);
+              }}
+            >
+              generate json
+            </Button>
+          ) : (
+            <Button
+              href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                exportJson
+              )}`}
+              download={`MTGDB_dump_${Date.now()}.json`}
+            >
+              download json
+            </Button>
+          )}
+
           <Button
             href={`data:application/octet-stream,${encodeURIComponent(
               Array.from(props.cards)
