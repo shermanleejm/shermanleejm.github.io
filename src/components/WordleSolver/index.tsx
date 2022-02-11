633948e309127e11e1474880083afe431caa74ec
shermanleejm <leesherman@live.com.sg>
2022-02-11 17:37:08 +0800

    lol

--- src/components/WordleSolver/index.tsx
+++ src/components/WordleSolver/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid, TextField, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import React from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import WORDS from './words';
 
 const WordleSolver = () => {
@@ -16,52 +17,72 @@ const WordleSolver = () => {
   const [wrongGuesses, setWrongGuesses] = useState('');
   const [words, setWords] = useState<string[]>([]);
   const [initial, setInitial] = useState<boolean>(true);
-  const [test, setTest] = useState('');
   const [showTest, setShowTest] = useState(false);
 
   const START_DATE = Date.parse('09 Feb 2022 00:00:00 GMT');
 
-  useEffect(() => {
-    setWords(WORDS);
-    let current =
-      (Math.floor((Date.now() - START_DATE) / (1000 * 60 * 60 * 24)) + 235) %
-      WORDS.length;
-    setTest(WORDS[current]);
-  }, []);
+  const test =
+    WORDS[
+      (Math.floor((Date.now() - START_DATE) / (1000 * 60 * 60 * 24)) + 235) % WORDS.length
+    ];
 
   function solve() {
     setInitial(false);
 
-    let expression =
-      (_1 === '' ? '.' : _1) +
-      (_2 === '' ? '.' : _2) +
-      (_3 === '' ? '.' : _3) +
-      (_4 === '' ? '.' : _4) +
-      (_5 === '' ? '.' : _5);
+    let wrongs = '';
+    let incorrects = [incorrect_1, incorrect_2, incorrect_3, incorrect_4, incorrect_5];
+    let incorrects_setters = [
+      setIncorrect_1,
+      setIncorrect_2,
+      setIncorrect_3,
+      setIncorrect_4,
+      setIncorrect_5,
+    ];
+    let guess_setters = [set_1, set_2, set_3, set_4, set_5];
+    let current = [_1, _2, _3, _4, _5];
+    let expression = '';
+
+    for (let i = 0; i < current.length; i++) {
+      let c = current[i];
+      if (!test.includes(c)) {
+        wrongs += c;
+        expression += '.';
+      } else if (test[i] !== c) {
+        incorrects[i] += c;
+        expression += '.';
+      } else {
+        expression += c;
+      }
+
+      if (c === test[i]) {
+        guess_setters[i](c);
+      } else {
+        guess_setters[i]('');
+      }
+    }
+    console.log(wrongs, incorrects, expression);
 
     const regex = new RegExp(expression);
     let tmp = WORDS.filter((w) => regex.test(w));
-    for (let c of wrongGuesses) {
+    for (let c of wrongGuesses + wrongs) {
       tmp = tmp.filter((w) => !w.includes(c));
     }
-    for (let c of incorrect_1) {
-      tmp = tmp.filter((w) => w.includes(c) && w[0] !== c);
-    }
-    for (let c of incorrect_2) {
-      tmp = tmp.filter((w) => w.includes(c) && w[1] !== c);
-    }
-    for (let c of incorrect_3) {
-      tmp = tmp.filter((w) => w.includes(c) && w[2] !== c);
-    }
-    for (let c of incorrect_4) {
-      tmp = tmp.filter((w) => w.includes(c) && w[3] !== c);
-    }
-    for (let c of incorrect_5) {
-      tmp = tmp.filter((w) => w.includes(c) && w[4] !== c);
+    setWrongGuesses(wrongGuesses + wrongs);
+    for (let i = 0; i < incorrects.length; i++) {
+      let inc = incorrects[i];
+      for (let c of inc) {
+        tmp = tmp.filter((w) => w.includes(c) && w[0] !== c);
+      }
+      incorrects_setters[i](inc);
     }
 
     setWords(tmp);
   }
+
+  const ref1 = React.useRef<HTMLInputElement>();
+  const ref2 = React.useRef<HTMLInputElement>();
+  const ref3 = React.useRef<HTMLInputElement>();
+  const ref4 = React.useRef<HTMLInputElement>();
 
   return (
     <div
@@ -122,35 +143,68 @@ const WordleSolver = () => {
             style={{ width: '2.5rem' }}
             value={_1}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              set_1(e.target.value.replace(/[^a-z]/, '').substring(0, 1));
+              set_1(
+                e.target.value
+                  .replace(/[^a-zA-Z]/, '')
+                  .substring(0, 1)
+                  .toLowerCase()
+              );
+              ref1.current?.focus();
             }}
           />
           <TextField
             style={{ width: '2.5rem' }}
             value={_2}
+            inputRef={ref1}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              set_2(e.target.value.replace(/[^a-z]/, '').substring(0, 1));
+              set_2(
+                e.target.value
+                  .replace(/[^a-zA-Z]/, '')
+                  .substring(0, 1)
+                  .toLowerCase()
+              );
+              ref2.current?.focus();
             }}
           />
           <TextField
             style={{ width: '2.5rem' }}
             value={_3}
+            inputRef={ref2}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              set_3(e.target.value.replace(/[^a-z]/, '').substring(0, 1));
+              set_3(
+                e.target.value
+                  .replace(/[^a-zA-Z]/, '')
+                  .substring(0, 1)
+                  .toLowerCase()
+              );
+              ref3.current?.focus();
             }}
           />
           <TextField
             style={{ width: '2.5rem' }}
             value={_4}
+            inputRef={ref3}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              set_4(e.target.value.replace(/[^a-z]/, '').substring(0, 1));
+              set_4(
+                e.target.value
+                  .replace(/[^a-zA-Z]/, '')
+                  .substring(0, 1)
+                  .toLowerCase()
+              );
+              ref4.current?.focus();
             }}
           />
           <TextField
             style={{ width: '2.5rem' }}
             value={_5}
+            inputRef={ref4}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              set_5(e.target.value.replace(/[^a-z]/, '').substring(0, 1));
+              set_5(
+                e.target.value
+                  .replace(/[^a-zA-Z]/, '')
+                  .substring(0, 1)
+                  .toLowerCase()
+              );
             }}
           />
         </Grid>
