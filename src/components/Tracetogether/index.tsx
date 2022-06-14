68813ce249c5706be409d26f475135946b6a3dc2
sherman windows <shermanleejm@gmail.com>
2022-06-15 00:22:54 +0800

    added spoof tracetogether

--- src/components/Tracetogether/index.tsx
+++ src/components/Tracetogether/index.tsx
@@ -1,4 +1,17 @@
+import { Divider, Grid, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import otter from './otter.gif';
+import moment from 'moment';
+
 const Tracetogether = () => {
+  const boldText = (text: string) => {
+    return (
+      <Typography>
+        <Box sx={{ fontWeight: 'bold' }}>{text}</Box>
+      </Typography>
+    );
+  };
+
   return (
     <div style={{ height: '100%' }}>
       <div
@@ -7,11 +20,50 @@ const Tracetogether = () => {
           left: '50%',
           position: 'absolute',
           transform: 'translate(-50%, -50%)',
-          backgroundColor: 'white',
+          backgroundColor: '#106e5d',
           color: 'black',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
+          flexDirection: 'column',
+          borderRadius: '10px',
+          width: '200px',
+          height: '400px',
         }}
       >
-        l l l l
+        <Grid
+          container
+          alignItems={'center'}
+          justifyContent={'center'}
+          direction="column"
+        >
+          <Box
+            component="img"
+            src={otter}
+            alt="otter"
+            sx={{
+              width: '60%',
+              height: 'auto',
+            }}
+          />
+          {/* <Divider style={{ color: 'white', width: '100%', paddingTop: '-20%' }} /> */}
+          {/* <hr style={{ color: 'white', width: '100%', marginTop: '-20%' }} /> */}
+
+          <Paper style={{ backgroundColor: 'white', color: 'black' }}>
+            <Grid
+              container
+              alignItems={'center'}
+              justifyContent={'center'}
+              direction="column"
+              spacing={1}
+              padding={1}
+            >
+              <Grid item>{boldText('GalaNight Check-in')}</Grid>
+              <Grid item>{boldText(moment().format('DD MMM, hh.mmA'))}</Grid>
+              <Grid item>{boldText('GALA NIGHT')}</Grid>
+            </Grid>
+          </Paper>
+        </Grid>
       </div>
     </div>
   );
