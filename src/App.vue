2aeea2297b2dbb6f9138a5c129d8f10d10f76f45
Sherman Lee <sherman.lee@accredify.io>
2022-06-24 16:18:13 +0800

    added preamble

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 }
 
 #app {
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Welcome to AB testing</div>
+  <div id="preamble">
+    <div>Welcome to AB testing</div>
+
+    <p>
+      The purpose of this survey is to determine which layout for skills and
+      proficiencies would be the most suitable for the general user.
+    </p>
+
+    <p>
+      The background for this is that certain skills do not start at level 1 and
+      certain skills do not end at level 6.
+    </p>
+
+    <p>
+      You will have the option of viewing 2 screens, A and B. You may fill in
+      the survey or one of our engineers will contact you.
+    </p>
+  </div>
 </template>
 
 <script>
@@ -8,4 +25,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#preamble {
+  width: 60%;
+}
+</style>
