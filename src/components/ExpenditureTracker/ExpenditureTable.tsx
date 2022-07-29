a937a4a951c8aa5d4beae04432623957250a1413
Sherman Mac <shermanleejm@gmail.com>
2022-07-30 01:17:11 +0800

    wip

--- src/components/ExpenditureTracker/ExpenditureTable.tsx
+++ src/components/ExpenditureTracker/ExpenditureTable.tsx
@@ -1,20 +1,31 @@
-import { Grid } from '@mui/material';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Grid, IconButton } from '@mui/material';
+import {
+  DataGrid,
+  GridCellEditCommitParams,
+  GridColDef,
+  GridValueGetterParams,
+} from '@mui/x-data-grid';
 import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ExpenditureTableType } from '../../database';
 import { State } from '../../state/reducers';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const ExpenditureTable = () => {
   const db = useSelector((state: State) => state.database);
 
   const [isLoading, setIsLoading] = useState(true);
+  const [nonce, setNonce] = useState(0);
   const [dateRange, setDateRange] = useState({
     startDate: dayjs().startOf('month').unix(),
     endDate: dayjs().unix(),
   });
   const [data, setData] = useState<Array<ExpenditureTableType>>([]);
+
+  const handleRowEdit = useCallback((params: GridCellEditCommitParams) => {
+    console.log(params);
+  }, []);
 
   const columns: GridColDef[] = [
     {
@@ -22,9 +33,19 @@ const ExpenditureTable = () => {
       valueGetter: (params: GridValueGetterParams) =>
         `${dayjs.unix(params.row.datetime).format('YYYY MMM DD')}`,
     },
-    { field: 'category' },
+    { field: 'category', editable: true },
     { field: 'name' },
     { field: 'amount' },
+    {
+      field: 'delete',
+      renderCell: (params: GridValueGetterParams) => {
+        return (
+          <IconButton>
+            <DeleteForeverIcon />
+          </IconButton>
+        );
+      },
+    },
   ];
 
   useEffect(() => {
@@ -47,7 +68,12 @@ const ExpenditureTable = () => {
         <Grid item>
           {!isLoading && (
             <div style={{ width: '90vw', height: '35vh' }}>
-              <DataGrid columns={columns} rows={data} />
+              <DataGrid
+                onCellEditCommit={handleRowEdit}
+                disableSelectionOnClick={true}
+                columns={columns}
+                rows={data}
+              />
             </div>
           )}
         </Grid>
