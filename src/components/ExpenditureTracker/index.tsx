a74f493c793212d2a38f71d7b9459da795daa66f
Sherman Mac <shermanleejm@gmail.com>
2023-05-17 22:17:51 +0800

    lol

--- src/components/ExpenditureTracker/index.tsx
+++ src/components/ExpenditureTracker/index.tsx
@@ -23,7 +23,7 @@ const ExpenditureTabs = [
 ];
 
 export default () => {
-  const [chosenTab, setChosenTab] = useState(ExpenditureTabs[1].name);
+  const [chosenTab, setChosenTab] = useState(ExpenditureTabs[0].name);
 
   return (
     <Box
